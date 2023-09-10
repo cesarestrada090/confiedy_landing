82fc8694df558b7f103889a50314131e6ec1f4b7
Estrada <cesarestrada090@gmail.com>
2023-09-10 15:37:12 -0500

    change tittle

--- edemy/components/_App/Layout.js
+++ edemy/components/_App/Layout.js
@@ -6,11 +6,11 @@ const Layout = ({ children }) => {
     return(
         <React.Fragment>
             <Head>
-                <title>eDemy - React Next.js Education LMS Template</title>
+                <title>Confiedy - Conecta con tu asesor de confianza</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <meta name="description" content="ConfiEdy es el mejor aliado para superar tus metas universitarias." />
                 <meta name="og:title" property="og:title" content="ConfiEdy es el mejor aliado para superar tus metas universitarias."></meta>
-                <meta name="twitter:card" content="eDemy - React Next.js Education LMS Template"></meta>
+                <meta name="twitter:card" content="Confiedy - Conecta con tu asesor de confianza"></meta>
                 <link rel="canonical" href="https://edemy-react.envytheme.com/"></link>
             </Head>
 
