d090eea3ed94a3fa85e249940392b9364489bf77
cesarestrada090 <cesarestrada090@gmail.com>
2024-03-19 23:38:17 -0500

    Update Professor.js

--- edemy/components/Professor.js
+++ edemy/components/Professor.js
@@ -8,7 +8,7 @@ import { LabelMedium } from 'baseui/typography'
 
 const Professor = ({ data }) => {
   console.log('data', data)
-
+  const tasks = data.tareas;
   return (
     <div
       className="profile-box ptb-100"
@@ -40,12 +40,23 @@ const Professor = ({ data }) => {
             <LabelMedium marginTop={'scale600'}>
               {`Horas Dictadas: ${data.numeroHorasDictadas}`}
             </LabelMedium>
+            <LabelMedium marginTop={'scale600'}>
+                 { tasks && tasks.length > 0 && <p><b>También realizo asesorias en trabajos en:</b></p>}
 
+               </LabelMedium>
+               {tasks.map((task) => (
+                   <div key={task.id}>
+                       <p>- {task.descripcion}</p>
+                   </div>
+               ))}
             <Block
               marginTop={'scale700'}
               display={'flex'}
               justifyContent={'center'}
             >
+
+
+
               <Link href="https://wa.me/51996115374">
                 <a className="default-btn">
                   <i className="flaticon-user"></i> ¡Inscribete! <span></span>
